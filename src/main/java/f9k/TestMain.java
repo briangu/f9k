a3ac9ec1278eafa2476cc7e8c67519674523cc3b
Brian Guarraci <bguarraci@linkedin.com>
2011-11-10 21:27:16 -0800

    add more tests

--- src/main/java/f9k/TestMain.java
+++ src/main/java/f9k/TestMain.java
@@ -4,10 +4,15 @@ package f9k;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import simplenlg.features.Feature;
+import simplenlg.features.Tense;
+import simplenlg.framework.CoordinatedPhraseElement;
 import simplenlg.framework.DocumentElement;
 import simplenlg.framework.NLGFactory;
 import simplenlg.lexicon.Lexicon;
 import simplenlg.lexicon.NIHDBLexicon;
+import simplenlg.phrasespec.NPPhraseSpec;
+import simplenlg.phrasespec.PPPhraseSpec;
 import simplenlg.phrasespec.SPhraseSpec;
 import simplenlg.realiser.english.Realiser;
 
@@ -36,7 +41,6 @@ public class TestMain
     Lexicon lexicon = Lexicon.getDefaultLexicon();
 
     lexicon = new NIHDBLexicon(getPwd() + DB_FILENAME);
-
     NLGFactory nlgFactory = new NLGFactory(lexicon);
     Realiser realiser = new Realiser(lexicon);
 
@@ -52,5 +56,51 @@ public class TestMain
 
     String output = realiser.realise(par1).getRealisation();
     System.out.println(output);
+
+    SPhraseSpec p = nlgFactory.createClause("Mary", "chase", "the monkey");
+    NPPhraseSpec place = nlgFactory.createNounPhrase("park");
+    place.setSpecifier("the");
+    PPPhraseSpec pp = nlgFactory.createPrepositionPhrase();
+    pp.addComplement(place);
+    pp.setPreposition("in");
+    p.addComplement(pp);
+    p.addComplement("very quickly");
+    p.addComplement("despite her exhaustion");
+    p.setFeature(Feature.NEGATED, "not");
+    output = realiser.realiseSentence(p);
+    System.out.println(output);
+
+    test1();
+  }
+
+  private static void test1()
+  {
+    Lexicon lexicon = new NIHDBLexicon(getPwd() + DB_FILENAME);
+    NLGFactory nlgFactory = new NLGFactory(lexicon);
+    Realiser realiser = new Realiser(lexicon);
+
+    SPhraseSpec s1 = nlgFactory.createClause("my cat", "like", "fish");
+    SPhraseSpec s2 = nlgFactory.createClause("my dog", "like", "big bones");
+    SPhraseSpec s3 = nlgFactory.createClause("my horse", "like", "grass");
+
+    CoordinatedPhraseElement c = nlgFactory.createCoordinatedPhrase();
+    // may revert to nlgFactory.createCoordinatedPhrase( ) ;
+    c.addCoordinate(s1);
+    c.addCoordinate(s2);
+    c.addCoordinate(s3);
+
+    String output = realiser.realiseSentence(c);
+    System.out.println(output);
+
+
+    SPhraseSpec p = nlgFactory.createClause("I", "be", "happy");
+    SPhraseSpec q = nlgFactory.createClause("I", "eat", "fish");
+
+    q.setFeature(Feature.COMPLEMENTISER, "because");
+    q.setFeature(Feature.TENSE, Tense.PAST);
+    p.addComplement(q);
+
+    String output4 = realiser.realiseSentence(p);  //Realiser created earlier
+    System.out.println(output4);
   }
 }
