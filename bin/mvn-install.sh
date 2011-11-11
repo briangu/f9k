fb2e63e09eda4ac07b5dec0358e029950587adcd
Brian Guarraci <bguarraci@linkedin.com>
2011-11-10 19:32:59 -0800

    use nih lex

--- bin/mvn-install.sh
+++ bin/mvn-install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 mvn install:install-file -Dfile=lib/simplenlgv4.3.jar -DgroupId=simplenlg -DartifactId=simplenlg -Dversion=4.3.0 -Dpackaging=jar
+# lexicon/lexAccess2011lite/lib/lexAccess2011dist.jar
+mvn install:install-file -Dfile=lexicon/lexAccess2011lite/lib/lexAccess2011dist.jar -DgroupId=lexAccess2011lite -DartifactId=lexAccess2011lite -Dversion=1.0.0 -Dpackaging=jar
 
 
--- src/main/java/f9k/TestMain.java
+++ src/main/java/f9k/TestMain.java
@@ -1,37 +1,56 @@
 package f9k;
 
-import simplenlg.framework.*;
-import simplenlg.lexicon.*;
-import simplenlg.realiser.english.*;
-import simplenlg.phrasespec.*;
-import simplenlg.features.*;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import simplenlg.framework.DocumentElement;
+import simplenlg.framework.NLGFactory;
+import simplenlg.lexicon.Lexicon;
+import simplenlg.lexicon.NIHDBLexicon;
+import simplenlg.phrasespec.SPhraseSpec;
+import simplenlg.realiser.english.Realiser;
 
-public class TestMain {
 
-    static String DB_FILENAME = "/home/bguarrac/scm/f9k/lexicon/lexAccess2011lite/data/HSqlDb/lexAccess2011.data";
+public class TestMain
+{
 
-    public static void main(String[] args) {
-	Lexicon lexicon = Lexicon.getDefaultLexicon();
+  static String DB_FILENAME = "/lexicon/lexAccess2011lite/data/HSqlDb/lexAccess2011.data";
 
-	lexicon = new NIHDBLexicon(DB_FILENAME);
-
-	NLGFactory nlgFactory = new NLGFactory(lexicon);
-	Realiser realiser = new Realiser(lexicon);
-
-        SPhraseSpec p1 = nlgFactory.createClause("Mary", "chase", "the monkey");
-        SPhraseSpec p2 = nlgFactory.createClause("The monkey", "fight back"); 
-        SPhraseSpec p3 = nlgFactory.createClause("Mary", "be", "nervous");
-
-        DocumentElement s1 = nlgFactory.createSentence(p1);
-        DocumentElement s2 = nlgFactory.createSentence(p2);
-        DocumentElement s3 = nlgFactory.createSentence(p3);
-
-	DocumentElement par1 = nlgFactory.createParagraph(Arrays.asList(s1, s2, s3));
-
-        String output = realiser.realise(par1).getRealisation();
-        System.out.println(output);
-
+  public static String getPwd()
+  {
+    File file = new File(".");
+    try
+    {
+      return file.getCanonicalPath();
     }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+    }
+    return "";
+  }
 
+  public static void main(String[] args)
+  {
+    Lexicon lexicon = Lexicon.getDefaultLexicon();
+
+    lexicon = new NIHDBLexicon(getPwd() + DB_FILENAME);
+
+    NLGFactory nlgFactory = new NLGFactory(lexicon);
+    Realiser realiser = new Realiser(lexicon);
+
+    SPhraseSpec p1 = nlgFactory.createClause("Mary", "chase", "the monkey");
+    SPhraseSpec p2 = nlgFactory.createClause("The monkey", "fight back");
+    SPhraseSpec p3 = nlgFactory.createClause("Mary", "be", "nervous");
+
+    DocumentElement s1 = nlgFactory.createSentence(p1);
+    DocumentElement s2 = nlgFactory.createSentence(p2);
+    DocumentElement s3 = nlgFactory.createSentence(p3);
+
+    DocumentElement par1 = nlgFactory.createParagraph(Arrays.asList(s1, s2, s3));
+
+    String output = realiser.realise(par1).getRealisation();
+    System.out.println(output);
+  }
 }
